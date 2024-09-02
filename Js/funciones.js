a6669971090573412ffa0701741f5896aa51f882
Manuel-Alquizar <malquizarr@gmail.com>
2024-09-02 12:12:10 -0500

    Mi primer commit

---
+++ Js/funciones.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    boto = document.getElementById("General");
+    subm(boto.click());
+});
+    
+function toggleMenu() {
+    let menu = document.getElementById("menuv");
+    let cab = document.getElementById("cabeza");
+    let conte = document.getElementById("contenedor");
+
+    if (menu.classList.contains("menu-close")) {
+        menu.classList.remove("menu-close");
+        cab.classList.remove("menu-close");
+        conte.classList.remove("menu-close");
+        cab.style.width="calc(100vw - 250px);";
+        conte.style.width="calc(100vw - 300px)";
+    } else {
+        menu.classList.add("menu-close");
+        cab.classList.add("menu-close");
+        conte.classList.add("menu-close");
+        conte.style.width="100vw";
+        cab.style.width="calc(100vw + 50px)";
+    }
+}
+
+function subm(event){
+    let pre = event.currentTarget.parentElement;
+    let opc = pre.querySelector('.submenu');
+    if (opc) {
+        if(opc.style.maxHeight == '800px'){
+            opc.style.maxHeight = '0px';
+        }else{
+            opc.style.maxHeight = '800px';
+        }
+    }
+
+    let fle = pre.querySelector(".flech");
+    if(fle){
+        if(fle.style.transform == `rotate(180deg)`){
+            fle.style.transform = `rotate(0deg)`;
+        }else{
+            fle.style.transform = `rotate(180deg)`;
+        }
+    }
+
+}
---
+++ Js/horarios/jshora.js
@@ -0,0 +1,88 @@
+document.addEventListener("DOMContentLoaded", function() {
+    recopilar_datos();
+});
+
+var horarios = [];
+var celdas = [];
+
+document.getElementById("select").addEventListener('change',recopilar_datos);
+
+function recopilar_datos(){
+
+    limpiar_tabla()
+
+    fetch('Datos/horarios.json')
+    .then(function(dat){
+        return dat.json();
+    })
+    .then(function(resp){
+
+        let semestre = document.getElementById("select").value;
+        
+        resp.forEach(data => {
+            if(data.codDocente == 8161078 && data.semestre == semestre){
+                horarios.push(data);
+            }
+        });
+        console.log(horarios);
+        cal_tabla();
+    })
+}
+
+function cal_tabla(){
+
+    const horas = ["", "07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
+    const dias = ["", "LU","MA","MI","JU","VI","SA","DO"];
+    let carga = 0;
+
+    horarios.forEach(datos =>{
+
+        let fil = horas.indexOf(datos.hora.slice(0,2));
+        let col = dias.indexOf(datos.dia);
+        let limi = horas.indexOf(datos.hora.slice(3));
+        let cant = limi - fil;
+
+        carga+=cant;
+
+        let tabla = document.getElementById("hora");
+        let celda = tabla.rows[fil].cells[col];
+
+        console.log(celda);
+
+        celda.rowSpan = cant;
+
+        for (let n=(fil+1); n < limi; n++){
+            tabla.rows[n].deleteCell(col);
+        }
+
+        celda.classList.add("hors");
+
+        let sentence = `<div><p><span class='material-icons'>person</span>(${datos.tope}) `+
+        `${datos.codCurso}-${datos.secCurso} /[${datos.codAula}](${datos.teopra}) </p></div>`;
+
+        celda.innerHTML = sentence;
+
+        celdas.push([celda,tabla, fil, limi]);
+    });
+
+    document.getElementById("CH").innerHTML = carga;
+
+    horarios=[];
+
+}
+
+function limpiar_tabla(){
+    
+    for(const cel of celdas){
+        cel[0].classList.remove("hors");
+        cel[0].innerHTML = "";
+        cel[0].rowSpan=1;
+
+        for (let n=(cel[2]+1); n < cel[3]; n++){
+            cel[1].rows[n].insertCell();
+        }
+
+    }
+
+    celdas = [];
+}
